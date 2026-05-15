156a5f6266cdfe6565cb7afb3aadacbace0f871d
abdulmosin178 <abdulmosin178@gmail.com>
2026-05-15 12:17:13 +0530

    code added'

---
+++ school_man/src/main/java/com/example/school_man/SchoolManApplication.java
@@ -0,0 +1,13 @@
+package com.example.school_man;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManApplication.class, args);
+	}
+
+}
---
+++ school_man/src/test/java/com/example/school_man/SchoolManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.school_man;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
